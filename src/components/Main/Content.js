21eb1954797e5676f6004610833b1af23f5c846e
comt-mix <comt-mix@gmail.com>
2022-11-26 19:27:20 +0900

    ♻️ Refactor: 코드스타일 및 레이아웃 수정

--- src/components/Main/Content.js
+++ src/components/Main/Content.js
@@ -5,35 +5,28 @@ import { COLORS } from "../../constants/colors";
 
 const Content = () => {
   return (
-    <Container>
-      <Wrapper>
-        <HeadText>실제 사용자의 이용 데이터를 한눈에 확인하세요.</HeadText>
-        <CheckList>
-          <List>
-            <FiCheck className="icon" /> 방문 횟수
-          </List>
-          <List>
-            <FiCheck className="icon" /> 평균 이용 시간
-          </List>
-          <List>
-            <FiCheck className="icon" /> 유입된 이전 사이트
-          </List>
-          <List>
-            <FiCheck className="icon" /> 이용자의 관심 키워드 파악(click)
-          </List>
-          <List>
-            <FiCheck className="icon" /> 이용 화면 녹화
-          </List>
-        </CheckList>
-      </Wrapper>
-    </Container>
+    <Wrapper>
+      <HeadText>실제 사용자의 이용 데이터를 한눈에 확인하세요.</HeadText>
+      <CheckList>
+        <List>
+          <FiCheck className="icon" /> 방문 횟수
+        </List>
+        <List>
+          <FiCheck className="icon" /> 평균 이용 시간
+        </List>
+        <List>
+          <FiCheck className="icon" /> 유입된 이전 사이트
+        </List>
+        <List>
+          <FiCheck className="icon" /> 이용자의 관심 키워드 파악(click)
+        </List>
+        <List>
+          <FiCheck className="icon" /> 이용 화면 녹화
+        </List>
+      </CheckList>
+    </Wrapper>
   );
 };
-
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -44,7 +37,7 @@ const Wrapper = styled.div`
   background-image: url("/images/ux-test.png");
   background-repeat: no-repeat;
   background-position: center;
-  background-size: 100%;
+  background-size: 90%;
 `;
 
 const HeadText = styled.div`
